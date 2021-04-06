179e6329e3dd4ac07d9ded528ad5bbeed92f00cc
fishjump <z1052525203@outlook.com>
2021-04-06 15:52:06 +0800

    update g++ version and add clang format

--- src/interrupt.cpp
+++ src/interrupt.cpp
@@ -1,18 +1,19 @@
 #include <interrupt/interrupt.hpp>
 
-namespace system::interrupt
-{
-    void entity::InterruptDescriptor::setHandler(uint64_t handler, uint16_t gdtSelector, InterruptDescriptorAttribute type, uint8_t privilege)
-    {
-        uint64_t address = handler;
-        selector = gdtSelector;
-        typeAttribute = (0b1 << 7 & 0b10000000) | (privilege << 5 & 0b01100000) | (0b0 << 4 & 0b00010000) | (type & 0b00001111);
-        handlerOffset1 = address & 0xffff;
-        handlerOffset2 = (address >> 16) & 0xffff;
-        handlerOffset3 = (address >> 32) & 0xffffffff;
+namespace system::interrupt {
+void entity::InterruptDescriptor::setHandler(
+    uint64_t handler, uint16_t gdtSelector,
+    InterruptDescriptorAttribute type, uint8_t privilege) {
+    uint64_t address = handler;
+    selector         = gdtSelector;
+    typeAttribute = (0b1 << 7 & 0b10000000) | (privilege << 5 & 0b01100000)
+                    | (0b0 << 4 & 0b00010000) | (type & 0b00001111);
+    handlerOffset1 = address & 0xffff;
+    handlerOffset2 = (address >> 16) & 0xffff;
+    handlerOffset3 = (address >> 32) & 0xffffffff;
 
-        ist = 0;
-        reserved = 0;
-    }
+    ist      = 0;
+    reserved = 0;
+}
 
 } // namespace system::interrupt
